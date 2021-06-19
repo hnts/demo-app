249c1a3582437ab50b66e8a6a1344703dd1d814e
Ryosuke Hanatsuka <hanatsuu@gmail.com>
2021-06-19 15:51:34 +0900

    Change path

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	handler := http.NewServeMux()
-	handler.HandleFunc("/api/hello", Hello)
+	handler.HandleFunc("/", Hello)
 	http.ListenAndServe("0.0.0.0:8080", handler)
 }
 
