d63e4eae89d11c67d3b97efc5f145c035185e05e
Ryosuke Hanatsuka <hanatsuu@gmail.com>
2021-06-28 14:37:36 +0900

    Add slow path

--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	handler := http.NewServeMux()
 	handler.HandleFunc("/", Hello)
-	http.ListenAndServe("0.0.0.0:8080", handler)
+	handler.HandleFunc("/slow", SlowPath)
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", handler))
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `Hello World Test`)
+	fmt.Fprintf(w, `Hello World`)
+}
+
+func SlowPath(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	fmt.Fprintln(w, "completed heavy task")
 }
